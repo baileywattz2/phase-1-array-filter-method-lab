443be0a052fe8ff02a34df3dfc9462de5c4a7e20
bailey watkins <bailey.watkins@rocketmail.com>
2021-05-03 18:20:12 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution here
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+//findMatching will use callback: drivers.filter
+ //for every driver we want to compare it to the
+ //povided string to see if equal. 
+ //.lowecase provided for case sensitivity
+
+function findMatching(drivers, string){
+  return drivers.filter(driver => 
+        driver.toLowerCase() === string.toLowerCase()
+    )
+}
+
+// fuzzyMatch takes the drivers array and a string as argument
+//were checking for every driver that has matching first letters
+//we do not want a string returned if there is no matches 
+
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(driver =>
+        driver[0] === string[0]
+    )
+}
+
+//matchName takes an array of objects and a string as an argument
+//it takes the drivers.filter callback function
+// to find the first names that match each other 
+
+
+function matchName(drivers, string){
+    return drivers.filter(driver =>
+        driver.name === string
+    )
+}
